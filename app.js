51db8cdcb39ae6efbc727c2d39f63d7f80793185
George Kokonas <g.kokwnas@gmail.com>
2022-12-15 21:57:21 +0200

    added switch player logic

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function GameBoard() {
   const cellsList = document.querySelectorAll(".cell");
   let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+  const player1 = "X";
+  const player2 = "O";
+  //true for player1 , otherwise false
+  let playingNow = true;
  
   function init() {
     // cells.forEach(cell => cell.textContent = "x")
@@ -13,7 +18,8 @@ function GameBoard() {
   //cellClicked()
   function getCellIndex() {
     const index = this.getAttribute("data-index");
-    gameBoard[index] = "x"
+    playingNow ? gameBoard[index] = player1 : gameBoard[index] = player2
+    playingNow = !playingNow;
     console.log(gameBoard);
   }
 
