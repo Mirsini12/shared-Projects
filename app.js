d356e42a1730d9e54ed1c507bb790fe243436b32
George Kokonas <g.kokwnas@gmail.com>
2022-12-15 22:36:13 +0200

    achieved to draw on screen the sign of each player with the updated board :)

--- app.js
+++ app.js
@@ -6,26 +6,31 @@ function GameBoard() {
   const player2 = "O";
   //true for player1 , otherwise false
   let playingNow = true;
- 
-  function init() {
-    // cells.forEach(cell => cell.textContent = "x")
-  }
 
   function getInput() {
-    cellsList.forEach(cell => cell.addEventListener('click',getCellIndex))
-    
+    cellsList.forEach((cell) => cell.addEventListener("click", getCellIndex));
   }
-  //cellClicked()
   function getCellIndex() {
     const index = this.getAttribute("data-index");
-    playingNow ? gameBoard[index] = player1 : gameBoard[index] = player2
-    playingNow = !playingNow;
+
+    drawOnScreen(index)
+  }
+
+  function drawOnScreen(index) {
+    if (playingNow) {
+      gameBoard[index] = player1; 
+      cellsList[index].textContent = player1
+      playingNow = !playingNow;
+    }
+    else {
+      gameBoard[index] = player2;
+      cellsList[index].textContent = player2;
+      playingNow = !playingNow;
+    }
     console.log(gameBoard);
   }
 
-  getInput()
+  getInput();
 }
-
-
 
 let x = GameBoard();
