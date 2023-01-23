8c361756b1590ce099d55b0b76537bb3c3993188
Mirsini12 <mirsininousi12@gmail.com>
2023-01-23 15:11:10 +0000

    changes-mirsini-tictactoejs

--- tic-tac-toe/tic-tac-toe-myrsini/tictactoe.js
+++ tic-tac-toe/tic-tac-toe-myrsini/tictactoe.js
@@ -6,87 +6,113 @@ const btnPlayerVsPlayer = document.getElementById("btnPlayerVsPlayer");
 btnPlayerVsPlayer.addEventListener("click", playAgainstPlayer);
 
 function playAgainstPlayer() {
+  //create a function switchWindows() to switch the game modes
   selectionWindow.style.display = "none";
   gameWindow.style.display = "block";
+  let running = true;
 
-  const playerText = document.getElementById("playerText");
+  //id playerText for a p that displays tic tac toe text and win message?
+  const header = document.getElementById("header");
   const restartBtn = document.getElementById("restartBtn");
-  const boxes = Array.from(document.getElementsByClassName("box"));
-  const congratulation = getComputedStyle(document.body).getPropertyValue(
-    "--winning-blocks"
-  );
 
-  const X_Letter = "X";
-  const O_Letter = "O";
-  let currentPlayer = X_Letter;
-  let counter = 0;
-  let spaces = Array(9).fill(null);
+  const boxes = document.querySelectorAll(".box");
+
+  const letterX = "X";
+  const letterO = "O";
+  let currentPlayer = letterX;
+
+  let tieCounter = 0;
+  let gameBoard = Array(9).fill(null);
+
   const startGame = function () {
     boxes.forEach((box) => box.addEventListener("click", boxClicked));
+    restartBtn.addEventListener("click", restart);
+
+    function boxClicked(b) {
+      if (running) {
+        updateBoard(b);
+        let winner = checkForWinner();
+
+        if (winner) {
+          markWinningBlocks(winner);
+        }
+        checkForTie();
+        switchPlayer();
+      }
+      return;
+    }
   };
 
-  function boxClicked(b) {
+  function updateBoard(b) {
     const id = b.target.id;
-    if (!spaces[id]) {
-      spaces[id] = currentPlayer;
+    if (!gameBoard[id]) {
+      gameBoard[id] = currentPlayer;
       b.target.innerText = currentPlayer;
-      counter += 1;
-    }
-
-    if (winner() !== false) {
-      playerText.innerHTML = currentPlayer + " has won!!!!";
-      let winning_blocks = winner();
-      winning_blocks.map(
-        (box) => (boxes[box].style.backgroundColor = congratulation)
-      );
-      return;
-    }
-
-    if (counter === 9) {
-      playerText.innerHTML = "It's a tie!!!!";
-      return;
-    }
-
-    if (currentPlayer === X_Letter) {
-      currentPlayer = O_Letter;
-    } else {
-      currentPlayer = X_Letter;
+      tieCounter += 1;
     }
   }
 
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+  function markWinningBlocks(winningBlocks) {
+    const winnerColor = getComputedStyle(document.body).getPropertyValue(
+      "--winning-blocks"
+    );
 
-  function winner() {
+    winningBlocks.map(
+      (box) => (boxes[box].style.backgroundColor = winnerColor)
+    );
+  }
+
+  function checkForWinner() {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
     for (const combo of winningCombinations) {
       let [a, b, c] = combo;
 
-      if (spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]) {
+      if (
+        gameBoard[a] &&
+        gameBoard[a] === gameBoard[b] &&
+        gameBoard[a] === gameBoard[c]
+      ) {
+        running = false;
         return [a, b, c];
       }
     }
     return false;
   }
 
-  restartBtn.addEventListener("click", restart);
+  function checkForTie() {
+    if (tieCounter === 9) {
+      //   playerText.innerHTML = "It's a tie!!!!";
+      return;
+    }
+  }
+
+  function switchPlayer() {
+    if (currentPlayer === letterX) {
+      currentPlayer = letterO;
+    } else {
+      currentPlayer = letterX;
+    }
+  }
 
   function restart() {
-    spaces.fill(null);
+    running = true;
+    gameBoard.fill(null);
     boxes.forEach((box) => {
       box.innerText = " ";
       box.style.backgroundColor = "";
     });
-    counter = 0;
-    currentPlayer = X_Letter;
-    playerText.innerHTML = "tic tac toe";
+    tieCounter = 0;
+    currentPlayer = letterX;
+    // playerText.innerHTML = "tic tac toe";
   }
 
   startGame();
